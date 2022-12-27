5dd9a3016d4a1211ba3ceea18cf1c017195b3745
Adarsh Gopalakrishnan <Adarsh.Gopalakrishnan@analog.com>
2022-12-27 15:36:50 +0530

    Modified day 23 solution to use hash map for slight speed improvement

--- 2022/day23/solution.cpp
+++ 2022/day23/solution.cpp
@@ -58,13 +58,16 @@ int main(void)
     int64_t cnt8 = 0, cnt9 = 0, cnt10 = 0, cnt11 = 0, cnt12 = 0, cnt13 = 0, cnt14 = 0;
     int64_t x1 = 0, y1 = 0, x2 = 0, y2 = 0;
 
+    int64_t nMaxXValue = 100000000;
+    int64_t nXYOffset = 10000;
+
     int64_t nMinValueRow;
     int64_t nMaxValueRow;
     int64_t nMinValueCol;
     int64_t nMaxValueCol;
 
     /* Coordinates of "#" into field */
-    vector<int64_t> field[2];
+    unordered_map<int64_t, int64_t> field;
     /* Coordinates of all possible movements into (field2[0], field2[1])
      * Store the corresponding value of original (X, Y) into (field2[2], field2[3])
      */
@@ -83,8 +86,9 @@ int main(void)
         {
             if (arr[cnt1][cnt3] == '#')
             {
-                field[0].push_back(cnt3);
-                field[1].push_back(cnt1);
+                int64_t xValue = cnt3 + nXYOffset;
+                int64_t yValue = cnt1 + nXYOffset;
+                field[(yValue * nMaxXValue) + xValue] = 1;
             }
         }
         /* Increment the count */
@@ -104,36 +108,86 @@ int main(void)
         field3[0].clear();
         field3[1].clear();
 
-        /* Loop through all the values in (field[0], field[1]) */
-        for (cnt5 = 0; cnt5 < field[0].size(); cnt5++)
+        /* Loop through all the values in field */
+        for (auto it : field)
         {
             /* Store the value of X and Y into seperate variables for ease */
-            int64_t x_value = field[0][cnt5];
-            int64_t y_value = field[1][cnt5];
+            int64_t x_value = it.first % nMaxXValue;
+            int64_t y_value = it.first / nMaxXValue;
+
             /* Store the co-ordinates of all the eight neighbouring #'s of a
              * given "#" */
             vector<int64_t> nEightNeigh[2];
-            /* Check if a # is cnt5 hash's neighbour */
-            for (cnt6 = 0; cnt6 < field[0].size(); cnt6++)
-            {
-                /* Ignore the self */
-                if (cnt6 == cnt5)
-                {
-                    continue;
-                }
-                /* Store the test value of (X, Y) into a new variable. Again this
-                 * is for ease */
-                int64_t x_newvalue = field[0][cnt6];
-                int64_t y_newvalue = field[1][cnt6];
 
-                /* Check if this is a neighbour by comparing the difference
-                 * between x and y co-ordinates */
-                if (abs(x_newvalue-x_value) < 2 && abs(y_newvalue-y_value) < 2)
-                {
-                    nEightNeigh[0].push_back(x_newvalue);
-                    nEightNeigh[1].push_back(y_newvalue);
-                }
+            /* Check for all 8 neighbours. See if one of them is #'s
+             * neighbour:
+             * (x_value + 1, y_value) - East
+             * (x_value - 1, y_value) - West
+             * (x_value, y_value + 1) - South
+             * (x_value, y_value - 1) - North
+             * (x_value + 1, y_value + 1) - South-East.
+             * (x_value + 1, y_value - 1) - North-East.
+             * (x_value - 1, y_value + 1) - South-West.
+             * (x_value - 1, y_value - 1) - North-West.
+             */
+            /* Linearise the value of (X,Y) */
+            /* Check East */
+            int64_t nXYNumber = (x_value + 1) + y_value * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value + 1);
+                nEightNeigh[1].push_back(y_value);
             }
+            /* Check West */
+            nXYNumber = (x_value - 1) + y_value * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value - 1);
+                nEightNeigh[1].push_back(y_value);
+            }
+            /* Check South */
+            nXYNumber = (x_value) + (y_value + 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value);
+                nEightNeigh[1].push_back(y_value + 1);
+            }
+            /* Check North */
+            nXYNumber = (x_value) + (y_value - 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value);
+                nEightNeigh[1].push_back(y_value - 1);
+            }
+            /* Check South-East */
+            nXYNumber = (x_value + 1) + (y_value + 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value + 1);
+                nEightNeigh[1].push_back(y_value + 1);
+            }
+            /* Check North-East */
+            nXYNumber = (x_value + 1) + (y_value - 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value + 1);
+                nEightNeigh[1].push_back(y_value - 1);
+            }
+            /* Check South-West */
+            nXYNumber = (x_value - 1) + (y_value + 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value - 1);
+                nEightNeigh[1].push_back(y_value + 1);
+            }
+            /* North-West */
+            nXYNumber = (x_value - 1) + (y_value - 1) * nMaxXValue;
+            if (field.find(nXYNumber) != field.end())
+            {
+                nEightNeigh[0].push_back(x_value - 1);
+                nEightNeigh[1].push_back(y_value - 1);
+            }
+
             /* If there is at least one neighbour */
             if (nEightNeigh[0].size() > 0)
             {
@@ -253,28 +307,28 @@ int main(void)
                  * anywhere, store the original # pos into field3 */
                 if (cnt6 == 4)
                 {
-                    field3[0].push_back(field[0][cnt5]);
-                    field3[1].push_back(field[1][cnt5]);
+                    field3[0].push_back(x_value);
+                    field3[1].push_back(y_value);
                 }
             }
             /* If could not find any neighbour, store the original # pos into
              * field3 */
             else
             {
-                field3[0].push_back(field[0][cnt5]);
-                field3[1].push_back(field[1][cnt5]);
+                field3[0].push_back(x_value);
+                field3[1].push_back(y_value);
             }
+
         }
         /* If all elements of field are in field3, we are done, the values cannot
          * move anywhere */
-        if (field[0].size() == field3[0].size())
+        if (field.size() == field3[0].size())
         {
             /* Break out of the loop and display the count */
             break;
         }
         /* To analyse, remove the all coordinates in field */
-        field[0].clear();
-        field[1].clear();
+        field.clear();
         /* Loop through all values in field2 */
         for (cnt5 = 0; cnt5 < field2[0].size(); cnt5++)
         {
@@ -296,21 +350,18 @@ int main(void)
             /* If there is no repitition, add the fresh value into field */
             if (cnt6 == field2[0].size())
             {
-                field[0].push_back(field2[0][cnt5]);
-                field[1].push_back(field2[1][cnt5]);
+                field[field2[0][cnt5] + field2[1][cnt5] * nMaxXValue] = 1;
             }
             /* Else, add the original value into field */
             else
             {
-                field[0].push_back(field2[2][cnt5]);
-                field[1].push_back(field2[3][cnt5]);
+                field[field2[2][cnt5] + field2[3][cnt5] * nMaxXValue] = 1;
             }
         }
         /* Add the remaining values that did not get a chance to move */
         for (cnt5 = 0; cnt5 < field3[0].size(); cnt5++)
         {
-            field[0].push_back(field3[0][cnt5]);
-            field[1].push_back(field3[1][cnt5]);
+            field[field3[0][cnt5] + field3[1][cnt5] * nMaxXValue] = 1;
         }
 
         /* If the count has reached 9, (counted from 0), compute the size of
@@ -318,16 +369,16 @@ int main(void)
         if (cnt4 == 9)
         {
             /* Start with min/max row number and column by the first value */
-            nMinValueRow = field[0][0];
-            nMaxValueRow = field[0][0];
-            nMinValueCol = field[1][0];
-            nMaxValueCol = field[1][0];
+            nMinValueRow = 10000000000000000;
+            nMaxValueRow = -10000000000000000;
+            nMinValueCol = 10000000000000000;
+            nMaxValueCol = -10000000000000000;
 
             /* Compute the min/max values of rows and columns */
-            for (cnt5 = 1; cnt5 < field[0].size(); cnt5++)
+            for (auto it : field)
             {
-                int64_t nXVal = field[0][cnt5];
-                int64_t nYVal = field[1][cnt5];
+                int64_t nXVal = it.first % nMaxXValue;
+                int64_t nYVal = it.first / nMaxXValue;
 
                 if (nMinValueCol > nXVal)
                 {
@@ -349,7 +400,7 @@ int main(void)
 
             /* Display the number dots by computing the area of rectange and
              * subtracting the number of #'s */
-            cout << "Part (a): " << (nMaxValueRow - nMinValueRow + 1) * (nMaxValueCol - nMinValueCol + 1) - field[0].size() << endl;
+            cout << "Part (a): " << (nMaxValueRow - nMinValueRow + 1) * (nMaxValueCol - nMinValueCol + 1) - field.size() << endl;
 
         }
     }
